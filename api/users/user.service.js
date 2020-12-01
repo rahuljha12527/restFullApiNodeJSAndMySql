ecf82fe984440715499e1c8946c7f35c2033c396
rahuljha661 <rahuljha12527@gmail.com>
2020-12-01 19:01:43 +0530

    add user service

---
+++ api/users/user.service.js
@@ -0,0 +1,27 @@
+const pool=require('../../config/database');
+
+
+module.exports={
+    create:(data,callBack)=>{
+        pool.query(
+            `insert into registration(emailId,password,firstName,lastName,StoryName)
+                  values()`,
+
+                  [
+                      data.first_name,
+                      data.last_name,
+                      data.emailId,
+                      data.password,
+                      data.StoryName,
+                      data.number
+                  ],(error,results,fields)=>{
+                      if(error){
+                     return callBack(error)
+                      }
+
+
+                      return callBack(null,results);
+                  }
+        );
+    }
+};
