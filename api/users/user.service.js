0299863f419e3a0a93913154dae430a20b5ad143
rahuljha661 <rahuljha12527@gmail.com>
2020-12-01 23:42:28 +0530

    add userController and user Service

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,27 +1,28 @@
-const pool=require('../../config/database');
+const pool = require("../../config/database");
 
-
-module.exports={
-    create:(data,callBack)=>{
-        pool.query(
-            `insert into registration(emailId,password,firstName,lastName,StoryName)
+module.exports = {
+  create: (data, callBack) => {
+    pool.query(
+      `insert into registration(emailId,password,firstName,lastName,StoryName)
                   values()`,
 
-                  [
-                      data.first_name,
-                      data.last_name,
-                      data.emailId,
-                      data.password,
-                      data.StoryName,
-                      data.number
-                  ],(error,results,fields)=>{
-                      if(error){
-                     return callBack(error)
-                      }
+      [
+        data.first_name,
+        data.last_name,
+        data.emailId,
+        data.password,
+        data.StoryName,
+        data.number,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
 
-
-                      return callBack(null,results);
-                  }
-        );
-    }
+        return callBack(null, results);
+      }
+    );
+  },
 };
+
+
--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app=express();
 
 
 
-app.get('/api',(req,res)=>{
-     res.json({
-         success:1,
-         message:"This is rest api working "
-     });
-});
+// app.get('/api',(req,res)=>{
+//      res.json({
+//          success:1,
+//          message:"This is rest api working "
+//      });
+// });
 
 app.listen(process.env.APP_PORT,()=>{
     console.log("Server up and running on PORT: ",process.env.APP_PORT);
