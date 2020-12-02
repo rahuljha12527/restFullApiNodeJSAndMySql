4ca13e3138a027c737a06c916ec7fcb4f8673a95
rahuljha661 <rahuljha12527@gmail.com>
2020-12-02 11:56:51 +0530

    cretated all the controller

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,31 +1,103 @@
-const {create}=require('./user.service');
+const {
+  create,
+  getUserByUserId,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("./user.service");
 
-const {genSaltSync,hashSync}=require("bcrypt");
+const { genSaltSync, hashSync } = require("bcrypt");
+const pool = require("../../config/database");
+
+module.exports = {
+  createUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection server",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUserByUserId:(req,res)=>{
+      const id=req.params.id;
+      getUserByUserId(id ,(err,results)=>{
+          if(err){
+              console.log(err);
+              return;
+          }
+
+          if(!result){
+              return res.json({
+                  success:0,
+                  message:"Record not found"
+              });
+
+          }
+          return res.json({
+              success:1,
+              data:results
+          });
+
+      });
+  },
+  getUsers:(req,res)=>{
+      getUsers((err,results)=>{
+          if(err){
+              console.log(err);
+              return;
+          }
+
+          return res.json({
+              success:1,
+              data:results
+          });
+      })
+  },
+
+  updateUser:(req,res)=>{
+      const body=req.body;
+      const salt=genSaltSync(10);
+      body.password=hashSync(body.password,salt);
+      updateUser(body,(err,results)=>{
+          if(err){
+              console.log(err);
+              return;
+          }
+
+          return res.json({
+              success:1,
+              message:"updated Successfully"
+          });
+      })
+  },
+
+  deleteUser:(data,callBack)=>{
+    pool.query(
+        `delete from registration where id=?`,
+        [data.id],
+        (error,results,fields)=>{
+            if(error){
+                return callBack(error);
+            }
+
+            return callBack(null,results[0]);
+        }
+    )
+  }
+  
 
 
-module.exports={
-    createUser:(req,res)=>{
-        const body=req.body;
-        const salt=genSaltSync(10);
-        body.password=hashSync(body.password,salt)
-        create(body,(err,results)=>{
-            if(err){
-                console.log(err);
-                return res.status(500).json({
-                    success:0,
-                    message:"Database connection server"
-                });
-            }   
-            return res.status(200).json({
-                success:1,
-                data:results
-            });
-            
-        })
-
-    }
-}
-
+};
 
 // Go with this link.
 
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -22,4 +22,62 @@ module.exports = {
       }
     );
   },
+  getUsers:callBack=>{
+    pool.query(
+      `select id,firstName,lastName,emailId,StoryName from registration`,
+      [],
+      (error,results,fields)=>{
+        if(error){
+          return callBack(error);
+        }
+
+        return callBack(null,results);
+      }
+    )
+  },
+  getUserByUserId:(id,callBack)=>{
+    pool.query(
+      `select id,firstName,lastName,emailId,StoryName from registration where id=?`,
+      [id],
+      (error,results,fields)=>{
+        if(error){
+          callBack(error);
+        }
+        return callBack(null,results[0]);
+      }
+    )    
+  },
+  updateUser:(data,callback)=>{
+    pool.query(
+      `update registration set firstName=? ,lastName=?,emailId=?,password=?,StoryName=? where id=?`,
+      [
+        data.emailId,
+        data.password,
+        data.firstName,
+        data.lastName,
+        data.StoryName,
+        data.id
+      ],
+      (error,results,fields)=>{
+        if(error){
+          callback(error);
+        }
+
+        return callback(null,results[0]);
+      }
+    )
+  },
+   deleteUser:(data,callBack)=>{
+     pool.query(
+       `delete from registration where id=?`,
+       [data.id],
+       (error,results,fields)=>{
+         if(error){
+            return  callBack(error);
+         }
+
+         return callBack(null,results[0]);
+       }
+     )
+   } 
 };
